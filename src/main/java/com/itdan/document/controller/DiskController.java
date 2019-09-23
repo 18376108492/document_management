3c038b0f5f0cc34996a667c2be01a1f224cebe90
Administrator <2207161187@qq.com>
2019-09-23 22:34:22 +0800

    文件树形显示测试

--- src/main/java/com/itdan/document/controller/DiskController.java
+++ src/main/java/com/itdan/document/controller/DiskController.java
@@ -5,16 +5,17 @@ import com.itdan.document.domain.FancytreeNode;
 import com.itdan.document.service.DocumentService;
 import com.itdan.document.utils.common.CookieUtils;
 import com.itdan.document.utils.common.JsonUtils;
-import org.apache.ibatis.annotations.Param;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.FileNotFoundException;
@@ -62,27 +63,34 @@ public class DiskController {
      * @throws Exception
      */
     @RequestMapping(value = "/disk/getDiskByName",
-                    method = RequestMethod.GET,
-                    produces = MediaType.TEXT_PLAIN_VALUE+";charset=UTF-8")
-    public String getDiskById(HttpServletRequest request,
-                              HttpServletResponse response) throws  Exception{//设置默认盘，不要问为什么设这就是喜欢
+                    method = RequestMethod.GET
+                    )
+    public ModelAndView getDiskById(HttpServletRequest request,
+                                    HttpServletResponse response) throws  Exception{
         //获取需要文件信息的磁盘名，将其先存储到cookie中
         String diskName=new String(request.getParameter("diskName").getBytes("iso-8859-1"),"UTF-8");
-        CookieUtils.setCookie(request, response, INIT_DISK_TOKEN, diskName);
+       ModelAndView view=new ModelAndView();
+       view.addObject(INIT_DISK_TOKEN,diskName);
         //然后跳转至文件界面
-        return "document";
+        view.setViewName("document");
+        return view;
     }
 
 
     @RequestMapping(value = "/disk/init_disk", method = RequestMethod.GET)
     @ResponseBody
-    public String initDisk(HttpServletRequest request) throws FileNotFoundException {
+    public String initDisk(HttpServletRequest request,
+                           HttpServletResponse response) throws FileNotFoundException {
         //获取要查询的磁盘名
-        String diskName = CookieUtils.getCookieValue(request, INIT_DISK_TOKEN);
-        System.out.println("diskName:" + diskName);
-        List<FancytreeNode> nodeList = documentService.listAllFile(diskName);
-        return JsonUtils.objectToJson(nodeList);
+       String diskName=request.getParameter(INIT_DISK_TOKEN);
+        System.out.println("diskName:"+diskName);
+       if (StringUtils.isNotBlank(diskName)){
+           List<FancytreeNode> nodeList = documentService.listAllFile(diskName);
+           return JsonUtils.objectToJson(nodeList);
+       }
+       return JsonUtils.objectToJson("");
     }
 
 
 }
+
